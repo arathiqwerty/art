c2e44de3bf161c342cd7cc822d45b2b78750d9ec
athira <arathi.ponnu49@gmail.com>
2018-07-19 10:04:59 +0530

    Initial Commit

---
+++ clctr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clctr
+//
+//  Created by codemac-04i on 19/07/18.
+//  Copyright © 2018 codemac-04i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
